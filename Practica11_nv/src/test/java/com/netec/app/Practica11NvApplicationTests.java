d3960549a51f402286c1df4606bf25c8b383bcfc
Blanca Escamilla <escamillablanca@yahoo.com.mx>
2023-04-17 06:09:12 -0600

    Actualizacion de practica 11

---
+++ Practica11_nv/src/test/java/com/netec/app/Practica11NvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netec.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica11NvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
